8140d86dedebe11d468bbe897c3f47d8e7790a79
myusoof <mohmedyusoof.as@cognizant.com>
2016-09-30 08:44:16 +0530

    test

--- src/main/scala/FirstScalaClass.scala
+++ src/main/scala/FirstScalaClass.scala
@@ -1,6 +1,19 @@
+import java.text.DateFormat
+import java.util.{Locale, Date}
+import javax.security.auth.callback.Callback
+
 /**
   * Created by yusoof on 30/9/16.
   */
-class FirstScalaClass {
-
+object FirstScalaClass {
+  def main(args: Array[String]) {
+    val now = new Date
+    val df = DateFormat.getDateInstance(3, Locale.FRANCE)
+    println(df format now)
+    println((1).+(2).*(3))
+    oncePerSecond(()=>println("testing"))
+  }
+    def oncePerSecond(callback: () =>Unit){
+      while(true){callback(); Thread.sleep(1000)}
+    }
 }
